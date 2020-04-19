fd074ac95cca5e23d3790195130c12ba3a4e76d8
rgxmg <rickgrimes9229@gmail.com>
2020-04-19 23:28:04 +0800

    changed

--- src/7.react/2.advanced/src/index.jsx
+++ src/7.react/2.advanced/src/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import PureComponentTest from "./2.pureComponent";
+import PureComponentTest from "./2.pureComonent/count";
 
 ReactDOM.render(<PureComponentTest />, document.getElementById("root"));
